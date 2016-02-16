692a687f570f110713f371faa7e1eaf9dc32486f
Lauxx <lm.nichols@hotmail.com>
2016-02-16 14:07:01 -0700

    we were able to post a bear via postman through the changes we made in server.js

--- server.js
+++ server.js
@@ -6,24 +6,63 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/animals');
 //object relational matter ORM = mongoose reaches into database; want to use animals database
 //run mongo(database) in terminal 'sudo mongod' - connects with database
+
+var Bear = require('./models/bear');
+
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-//^^mounting middleware
-var port = process.env.PORT || 8080;
-//^^set a port
-var router = express.Router();
-//^^get an instance of router
+app.use(bodyParser.json());  //mounting middleware
+
+var port = process.env.PORT || 8080; //set a port
+
+var router = express.Router(); //get an instance of router
+
+router.use(function(req, res, next){
+	console.log("Something is happening");
+	next();//make sure we go to the next routes and don't stop here
+});
+
+
 router.get('/', function(req, res){
 	res.json({message: 'Good job Lauren!'});
-});
-//^^test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+}); //^^test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 
 
 //MORE ROUTES FOR OUR API WILL HAPPEN HERE!
 
-app.use('/api', router);
-//^^REGISTER OUR ROUTES: all of our routes will be prefixed with /api
 
-app.listen(port);
-//^^START THE SERVER
+
+
+
+router.route('/bears')
+	.post(function(req, res){
+
+		var bear = new Bear();
+
+		bear.name = req.body.name;
+		bear.age = req.body.age;
+		bear.gender = req.body.gender;
+
+		bear.save(function (err, bear){
+			if(err){
+				console.log(err)//do something
+			}else{
+				res.json(bear)//do something
+			}
+		});
+
+	})
+	.get(function(req, res){
+		res.json({title: 'I am trying to GET'})
+	});
+
+
+
+
+
+app.use('/api', router); //REGISTER OUR ROUTES: all of our routes will be prefixed with /api
+
+app.listen(port); //START THE SERVER
 console.log("Magic happens on port " + port);
+
+
+
