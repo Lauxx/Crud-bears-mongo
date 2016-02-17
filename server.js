50039376badcfdc7472c338fe63d09d4d7b4ab42
Lauxx <lm.nichols@hotmail.com>
2016-02-17 11:23:37 -0700

    we created a new directory with a file named bears.js and refactored our code so that the routes are now in the bears.js file with a link between server.js and bears.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ mongoose.connect('mongodb://localhost/animals');
 //object relational matter ORM = mongoose reaches into database; want to use animals database
 //run mongo(database) in terminal 'sudo mongod' - connects with database
 
+var bearRouter= require('./routes/bears');
+
 var Bear = require('./models/bear');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -27,83 +29,7 @@ router.get('/', function(req, res){
 }); //^^test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 
 
-//MORE ROUTES FOR OUR API WILL HAPPEN BELOW!
-
-
-router.route('/bears')
-	.post(function(req, res){
-
-		var bear = new Bear();
-
-		bear.name = req.body.name;
-		bear.age = req.body.age;
-		bear.gender = req.body.gender;
-
-		bear.save(function (err, bear){
-			if(err){
-				console.log(err)//do something
-			}else{
-				res.json(bear)//do something
-			}
-		});
-
-	})
-
-	.get(function(req, res){
-		Bear.find(function(err, bears){
-			if(err){
-				console.log(err)
-			}else{
-				res.json(bears)
-			}
-		})
-		
-	});
-
-
-router.route('/bears/:bear_id')//finding bears by id
-	.get(function(req, res){
-		Bear.findById(req.params.bear_id, function(err, bear){
-			if(err){
-				console.log(err)
-			}else{
-				res.json(bear)
-			}
-		})
-	})
-	.put(function(req, res){
-		Bear.findById(req.params.bear_id, function(err, bear){
-			if(err){
-				console.log(err)
-			}else{
-				bear.name = req.body.name ? req.body.name : bear.name; //terinary statement
-				bear.age = req.body.age ? req.body.age : bear.age;
-				bear.gender = req.body.gender ? req.body.gender : bear.gender;
-
-				bear.save(function(err){
-					if(err){
-						console.log(err)
-					}else{
-						res.json({title: 'bear updated'})
-					}
-				})
-			}
-		})
-	})
-
-	.delete(function(req, res){
-		Bear.remove({_id: req.params.bear_id}, function(err, bear){
-			if(err){
-				console.log(err)
-			}else{
-				res.json({title: 'bear deleted'})
-			}
-		})
-	})
-
-
-
-app.use('/api', router); //REGISTER OUR ROUTES: all of our routes will be prefixed with /api
+app.use('/api', bearRouter); //REGISTER OUR ROUTES: all of our routes will be prefixed with /api
 
 app.listen(port); //START THE SERVER
 console.log("Magic happens on port " + port);
