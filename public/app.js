eac617f6fbd0d65239bb07edb662d55007060ec5
Lauxx <lm.nichols@hotmail.com>
2016-02-18 14:13:42 -0700

    we are now able to append a new bear and delete same bear in the same window

--- public/app.js
+++ public/app.js
@@ -23,8 +23,41 @@ var addBear = function(){
 	event.preventDefault();
 
 	var name = $('#name').val();
-	
-	alert("You entered " + name);
+	var age = $('#age').val();
+	var gender = $('#gender').val();
+
+	var $table = $('#bearTable');
+
+	var bear = {};
+	bear.name = name;
+	bear.age = age;
+	bear.gender = gender;
+
+	$.ajax({
+		url: 'api/bears/', 
+		method: 'POST',
+		data: bear, 
+	}).done(function(data){
+		console.log('I posted a bear!', data);
+
+		$table.append('<tr id=' + data._id + '>\
+			<td>' + data.name + '</td>\
+			<td>' + data.age +'</td>\
+			<td>' + data.gender +'</td>\
+			<td><button class="btn btn-danger deleteBear">Delete</button></td>\
+			</tr>'
+					);
+		$('.deleteBear').on('click', deleteBear);
+
+	});
+
+	$('#name').val('');
+	$('#age').val('');
+	$('#gender').val('');
+
 }
+
 $('#addBear').on('click', addBear);
-$('.deleteBear').on('click', deleteBear);
+
+
+
